bea50a06d44d6914148305021cb32c2e8de58955
MohamedBassem <medoox240@gmail.com>
2015-07-31 01:51:54 +0200

    Adding template generation

--- main.go
+++ main.go
@@ -3,17 +3,30 @@ package main
 import (
 	"bufio"
 	"errors"
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"os"
 	"regexp"
 	"strings"
+	"time"
 )
 
 func checkError(err error) {
 	if err != nil {
 		log.Fatal("ERR: " + err.Error())
 	}
+}
+
+func randString(length int) string {
+	var ret string
+	runes := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPKRSTUVWXYZ0123456789")
+	for i := 0; i < length; i++ {
+		ret += string(runes[rand.Intn(len(runes))])
+	}
+	return ret
 }
 
 func extractFilePackage(filename string) (string, error) {
@@ -39,6 +52,7 @@ func extractFilePackage(filename string) (string, error) {
 }
 
 func extractPackage() string {
+	rand.Seed(time.Now().Unix())
 	workingDir, err := os.Getwd()
 	checkError(err)
 	dirFiles, err := ioutil.ReadDir(workingDir)
@@ -72,3 +86,33 @@ func extractPackage() string {
 	}
 	return packageName
 }
+
+func main() {
+	dryRun := flag.Bool("dry-run", false, "Print the test file")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) == 0 {
+		log.Fatal("Err: At least a function name should be given")
+	}
+
+	packageName := extractPackage()
+	functionName := args[0]
+	functionArgs := ""
+	if len(args) > 1 {
+		functionArgs = strings.Join(args[1:], ", ")
+	}
+
+	fileContent := fmt.Sprintf(templateString, packageName, randString(10), functionName, functionArgs)
+	if *dryRun {
+		fmt.Println(fileContent)
+	}
+}
+
+var templateString = `package %v
+import "testing"
+
+func Test_%v(t *testing.T) {
+	  %v(%v)
+}
+`
